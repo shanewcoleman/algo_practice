3acbed1c950f524fbfef4502a7356165e2cf2401
Shane Coleman <shane.coleman@sas.com>
2021-06-07 16:37:53 -0400

    cleanup

--- clone_randomly_linked_list/clone_randomly_linked_list.py
+++ clone_randomly_linked_list/clone_randomly_linked_list.py
@@ -1,7 +1,7 @@
 import random
 class Node:
-    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
-        self.data = int(x)
+    def __init__(self, x = None, next: 'Node' = None, random: 'Node' = None):
+        self.data = x
         self.next = next
         self.random = random
 
@@ -24,7 +24,7 @@ def copyRandomList(head):
 
     # Set each copy's .random
     node = head
-    while node.random:
+    while node and node.random:
         node.next.random = node.random.next #and node.random.next
         node = node.next.next
 
@@ -44,27 +44,27 @@ def copyRandomList(head):
 ###
 def create_rand_ll(n):
     llist = LinkedList()
-    llist.head = Node(0)
+    llist.head = Node()
     node = llist.head
-    rand_set = set()
-    while len(rand_set) < n:
+    rand_set = set() # Initialize set data structure so we don't get duplicate items
+    while len(rand_set) < n: # Make sure the set has the appropirate number of items
         rand = random.randint(0, n)
         rand_set.add(rand)
     l = list(rand_set)
-    random.shuffle(l)
-    ll = []
+    random.shuffle(l) #Shuffle our list so the data isn't ordered and boring
+    ll = [] # Our data structure here is going to be a list of linked lists, that way we have an index to point to for our .random creation
     ll.append(node)
 
     for i in l:
+        node.next = Node()
         node.data = i
-        node.next = Node(i)
         node = node.next
-        ll.append(node)
-    random.shuffle(l)
+        ll.append(node) # We've created the nodes and added them to our list
+    random.shuffle(l) # Shuffle it up again so we can point to random elements in our list
     node = llist.head
     
     for i in l:
-        node.random = ll[i]
+        node.random = ll[i]#Each element in l points to a random node now, so the .random will in fact point to a random item in the linked list
         node = node.next
     return llist.head
 
@@ -73,7 +73,7 @@ def create_rand_ll(n):
 #
 #Create and copy the list 
 ####
-head = create_rand_ll(10)
+head = create_rand_ll(7)
 
 copy = copyRandomList(head)
 
@@ -81,13 +81,13 @@ print("Original list")
 while head:
     print(vars(head))
     if head.random:
-        print(head.random.data)
+       print(head.next.data)
     head = head.next
 
 print("Copied list")    
 while copy:
     print(vars(copy))
-    if copy.random:
-        print(copy.random.data)
+   # if copy.random:
+    #    print(copy.random.data)
     copy = copy.next
 
